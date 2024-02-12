b2be73a904eb8a03a4a945b1654ef3e46dc271bc
jnapolitano7 <joseph.napolitano@gtri.gatech.edu>
2024-02-12 14:20:46 -0700

    Javadoc Image Test

--- src/main/java/edu/gatech/gtri/obm/Main.java
+++ src/main/java/edu/gatech/gtri/obm/Main.java
@@ -1,6 +1,6 @@
 package edu.gatech.gtri.obm;
 /** 
- * Test Changed on Feb 12, 2024 
+ * Test Changed on Feb 12, 2024\n
  * <img src="doc-files/seq.png" alt="Example of the application sequence"/>
  */
 public class Main {}
