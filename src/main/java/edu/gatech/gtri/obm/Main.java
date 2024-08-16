986eb1f06c43a516a55dd8605e1b4b3b81950b07
jnapolitano7 <joseph.napolitano@gtri.gatech.edu>
2024-08-16 11:13:00 -0600

    Update Main.java

--- src/main/java/edu/gatech/gtri/obm/Main.java
+++ src/main/java/edu/gatech/gtri/obm/Main.java
@@ -1,3 +1,6 @@
 package edu.gatech.gtri.obm;
-/** Created by jdoerr3 on 2023-04-26. Purpose: Main class for OBM translator Assumptions: */
+/**
+ * Created by jdoerr3 on 2023-04-26. Purpose: Main class for OBM translator Assumptions: <img
+ * src="doc-files/seq.svg" alt="Example of the application sequence"/>
+ */
 public class Main {}
