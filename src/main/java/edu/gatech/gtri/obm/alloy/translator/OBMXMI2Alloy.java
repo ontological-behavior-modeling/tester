8b7e9e50d5308e7b12adc33bdf52cab552238ba2
jnapolitano7 <joseph.napolitano@gtri.gatech.edu>
2024-08-19 11:15:49 -0600

    puml changes

--- src/main/java/edu/gatech/gtri/obm/alloy/translator/OBMXMI2Alloy.java
+++ src/main/java/edu/gatech/gtri/obm/alloy/translator/OBMXMI2Alloy.java
@@ -64,8 +64,8 @@ public final class OBMXMI2Alloy {
 
   /**
    * A constructor to set the given alloyLibPath as an instance variableImage Below 1 Image below
-   * 
-   * <p><img src="doc-files/OBMXMI2Alloy.svg"/></p>
+   *
+   * <p><img src="doc-files/OBMXMI2Alloy.svg"/>
    *
    * @param alloyLibPath - An absolute path name string for the required library folder containing
    *     Transfer.als and utilities(folder) necessary for translation.
@@ -75,9 +75,9 @@ public final class OBMXMI2Alloy {
   }
 
   /**
-   * loading xmiFile to preparing for translation. Updated
+   * loading xmiFile to preparing for updated translation.
    *
-   * <p><img src="doc-files/OBMXMI2Alloy_loadXmiFile.svg"/></p>
+   * <p><img src="doc-files/OBMXMI2Alloy_loadXmiFile.svg"/>
    *
    * @param xmiFile - xmiFile containing classes you like to translate to an alloy file.
    * @return true if successful, otherwise return false
@@ -117,7 +117,7 @@ public final class OBMXMI2Alloy {
    * Create an alloy output file of the qualifideName class/behavior model in the xml file. If this
    * method return false, you may use getErrorMessages() to know why cause failure.
    *
-   * <p><img src="doc-files/OBMXMI2Alloy_createAlloyFile.svg"></p>
+   * <p><img src="doc-files/OBMXMI2Alloy_createAlloyFile.svg">
    *
    * @param qualifiedName of a UML:Class for translation (ie.,
    *     Model::FoodService::OFSingleFoodService)
